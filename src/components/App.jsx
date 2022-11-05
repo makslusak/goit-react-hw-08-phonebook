520d659077fed7130d7601d4c41f5bb2b41ba2f0
Maks <106471649+makslusak@users.noreply.github.com>
2022-11-05 18:08:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+
 import css from './ContactForm/ContactForm.module.css';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -7,17 +7,6 @@ import { Filter } from './Filter/Filter';
 import { useSelector } from 'react-redux';
 
 export const App = () => {
-  const contacts = useSelector(state => state.contacts);
-
-  // useEffect(() => {
-  //   const parsedContacts = JSON.parse(localStorage.getItem('contactList'));
-  //   // dispatch(fromLocalStorage(parsedContacts));
-  //   // eslint-disable-next-line
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contactList', JSON.stringify(contacts));
-  // }, [contacts]);
 
   return (
     <div className={css.container}>
