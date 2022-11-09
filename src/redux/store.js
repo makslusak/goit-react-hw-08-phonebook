b3746c828b8cc2ce9a95384d9eea3ca9e423c458
makslusak <106471649+makslusak@users.noreply.github.com>
2022-11-09 17:37:06 +0200

    Kiled Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 import storage from 'redux-persist/lib/storage';
 import { contactReducer } from './slice';
@@ -19,17 +9,7 @@ export const persistConfig = {
   whitelist: ['contacts'],
 };
 
-const persistedReducer = persistReducer(persistConfig, contactReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: contactReducer,
   devTools: true,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
